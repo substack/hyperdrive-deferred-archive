8c3bcba1a6b2a5cb534a413976ff16a07fa2ca84
James Halliday <mail@substack.net>
2016-06-18 00:33:22 -0500

    test .get() and .list() too

--- test/defer.js
+++ test/defer.js
@@ -5,7 +5,7 @@ var hyperdrive = require('hyperdrive')
 var concat = require('concat-stream')
 
 test('defer', function (t) {
-  t.plan(1)
+  t.plan(6)
   var archive = defer()
   var w = archive.createFileWriteStream('hello.txt')
   w.end('whatever')
@@ -14,8 +14,19 @@ test('defer', function (t) {
       .pipe(concat({ encoding: 'string' }, function (body) {
         t.equal(body, 'whatever')
       }))
+    archive.list(function (err, entries) {
+      t.error(err)
+      t.deepEqual(entries.map(ename), ['hello.txt'])
+    })
+    archive.get(0, function (err, entry) {
+      t.error(err)
+      t.equal(entry.name, 'hello.txt')
+      t.equal(entry.length, 8)
+    })
   })
 
   var drive = hyperdrive(memdb())
   archive.setArchive(drive.createArchive({ live: true }))
 })
+
+function ename (entry) { return entry.name }
