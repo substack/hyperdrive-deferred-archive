7d4d5cb131bd11c3ab3b6b5a9f2606a6c945972c
James Halliday <mail@substack.net>
2016-06-18 00:30:40 -0500

    implementation sourced from hyperdrive-prefix, test passes

---
+++ index.js
@@ -0,0 +1,66 @@
+var inherits = require('inherits')
+var EventEmitter = require('events').EventEmitter
+var duplexify = require('duplexify')
+var path = require('path')
+
+inherits(Defer, EventEmitter)
+module.exports = Defer
+
+function Defer (archive) {
+  if (!(this instanceof Defer)) return new Defer(archive)
+  EventEmitter.call(this)
+  if (archive) this.setArchive(archive)
+}
+
+Defer.prototype.setArchive = function (archive) {
+  this._archive = archive
+  this.key = archive.key
+  this.emit('_archive', archive)
+}
+
+Defer.prototype._getArchive = function (fn) {
+  if (this._archive) fn(this._archive)
+  else this.once('_archive', fn)
+}
+
+Defer.prototype.append = function (entry, cb) {
+  this._getArchive(function (archive) {
+    archive.append(entry, cb)
+  })
+}
+
+Defer.prototype.get = function (index, cb) {
+  this._getArchive(function (archive) {
+    archive.get(index, cb)
+  })
+}
+
+Defer.prototype.download = function (index, cb) {
+  this._getArchive(function (archive) {
+    archive.download(index, cb)
+  })
+}
+
+Defer.prototype.list = function (opts, cb) {
+  var d = duplexify.obj()
+  this._getArchive(function (archive) {
+    d.setReadable(archive.list(opts, cb))
+  })
+  return d
+}
+
+Defer.prototype.createFileReadStream = function (entry) {
+  var d = duplexify()
+  this._getArchive(function (archive) {
+    d.setReadable(archive.createFileReadStream(entry))
+  })
+  return d
+}
+
+Defer.prototype.createFileWriteStream = function (entry) {
+  var d = duplexify()
+  this._getArchive(function (archive) {
+    d.setWritable(archive.createFileWriteStream(entry))
+  })
+  return d
+}
---
+++ test/defer.js
@@ -0,0 +1,21 @@
+var test = require('tape')
+var defer = require('../')
+var memdb = require('memdb')
+var hyperdrive = require('hyperdrive')
+var concat = require('concat-stream')
+
+test('defer', function (t) {
+  t.plan(1)
+  var archive = defer()
+  var w = archive.createFileWriteStream('hello.txt')
+  w.end('whatever')
+  w.once('finish', function () {
+    archive.createFileReadStream('hello.txt')
+      .pipe(concat({ encoding: 'string' }, function (body) {
+        t.equal(body, 'whatever')
+      }))
+  })
+
+  var drive = hyperdrive(memdb())
+  archive.setArchive(drive.createArchive({ live: true }))
+})
